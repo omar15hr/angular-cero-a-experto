963879d6a9c4b6739a129b6a4bbf0fefa9f7090b
omar <omarhrn@gmail.com>
2024-12-03 18:54:10 -0300

    Recibir del padre con un input

--- 02-bases/src/app/dbz/components/list/list.component.ts
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
-export class ListDBZComponent { }
+export class ListDBZComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }]
+}
--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Character } from "../interfaces/character.interface";
 
 @Component({
   selector: "app-dbz-main-page",
@@ -6,5 +7,19 @@ import { Component } from "@angular/core";
 })
 
 export class MainPageComponent {
-  title = "DBZ Main Page";
+ 
+  public characters: Character[] = [{
+    name: "Krillin",
+    power: 1000
+  },
+  {
+    name: "Goku",
+    power: 9500
+  },
+  {
+    name: "Vegeta",
+    power: 9000
+  },
+];
+
 }
