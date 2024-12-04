8bb6a6ad2a3941a807ae69637553d20eea331f89
omar <omarhrn@gmail.com>
2024-12-04 14:37:45 -0300

    Resolución de tarea y servicio privado

--- 02-bases/src/app/dbz/components/list/list.component.ts
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -15,9 +15,10 @@ export class ListDBZComponent {
   }]
 
   @Output()
-  public onDeleteId: EventEmitter<number> = new EventEmitter();
+  public onDeleteId: EventEmitter<string> = new EventEmitter();
 
-  onDeleteCharacter(index: number): void {
-    this.onDeleteId.emit(index);
+  onDeleteCharacter(id?: string): void {
+    if (!id) return;
+    this.onDeleteId.emit(id);
   }
 }
