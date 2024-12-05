cddf5e1ae4756add4eabbd662665ac887e664917
omar <omarhrn@gmail.com>
2024-12-05 12:06:18 -0300

    Validaciones al servicio

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -13,7 +13,19 @@ export class GifsService {
     return [...this._tagsHistory];
   }
 
-  searchTag(tag: string): void {
+  private organizeHistory(tag: string): void {
+    tag = tag.toLowerCase();
+
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter(oldTag => oldTag !== tag);
+    }
+
     this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
+  }
+
+  searchTag(tag: string): void {
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
   }
 }
