b1f638243e8d4883945bd00d176b02efb4808045
omar <omarhrn@gmail.com>
2024-12-06 11:48:18 -0300

    Pensemos en componentes con tarea incluida

---
+++ 03-gifs-app/src/app/gifs/components/gifs-card/gifs-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interfaces';
+
+@Component({
+  selector: 'gifs-card',
+  templateUrl: './gifs-card.component.html',
+  styleUrl: './gifs-card.component.css'
+})
+export class GifsCardComponent implements OnInit {
+
+  ngOnInit(): void {
+    if ( !this.gif ) throw new Error('Gif is required');
+  }
+  
+  @Input()
+  public gif!: Gif;
+
+
+}
