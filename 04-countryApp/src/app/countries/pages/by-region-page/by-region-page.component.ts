1c056245e2ed211659b45708c0528eca1a171ccb
omar <omarhrn@gmail.com>
2024-12-09 12:02:26 -0300

    Tarea pais y region

--- 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-bu-region-page',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './by-region-page.component.css'
 })
 export class ByRegionPageComponent {
+
+  public countries: Country[] = [];
+
+  constructor(private countriesService: CountriesService) { }
+
+  searchByRegion(term: string): void {
+    this.countriesService.searchRegion( term )
+      .subscribe( countries => {
+        this.countries = countries;
+      } );
+  }
 
 }
--- 04-countryApp/src/app/countries/services/countries.service.ts
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -9,12 +9,28 @@ import { Country } from '../interfaces/country';
 
 export class CountriesService {
 
-  private apiUrl: string = 'https://restcountries.com/v3.1'
+  private apiUrl: string = 'https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) { }
 
   searchCapital( term: string ): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( () => of([]) ),
+      );
+  }
+
+  searchCountry( term: string ): Observable<Country[]> {
+    const url = `${this.apiUrl}/name/${term}`;
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( () => of([]) ),
+      );
+  }
+
+  searchRegion( region: string ): Observable<Country[]> {
+    const url = `${this.apiUrl}/region/${region}`;
     return this.http.get<Country[]>( url )
       .pipe(
         catchError( () => of([]) ),
