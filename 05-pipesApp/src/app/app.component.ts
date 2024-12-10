7ea7f60103adcfaa730985394d71248ac3a3ed24
omar <omarhrn@gmail.com>
2024-12-10 11:42:41 -0300

    Instalacion primeng

--- 05-pipesApp/src/app/app.component.ts
+++ 05-pipesApp/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'oMaR';
+export class AppComponent implements OnInit {
+  title = 'pipesApp';
+
+  constructor( private primengConfig: PrimeNGConfig) { }
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+  }
 }
