c81ae91d951db8a82200183cbc356df6672a0d85
omar <omarhrn@gmail.com>
2024-12-11 11:04:16 -0300

    i18nselect y plural pipe

--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18n Select
+  public name: string = 'Omar Hernandez';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    'male' : 'invitarlo',
+    'female' : 'invitarla'
+  }
+
+  changeClient(): void {
+    this.name = 'Brina Diaz';
+    this.gender = 'female';
+  }
+
+  // i18n Plural
+  public clients: string[] = ['Omar', 'Brina', 'Maria', 'Juan', 'Pedro', 'Luis', 'Carlos', 'Daniel', 'Fernando', 'Gabriel'];
+  public clientsMap = {
+    '=0': 'no hay clientes esperando',
+    '=1': 'tenemos un cliente esperando.',
+    'other': 'tenemos # clientes esperando.'
+
+  }
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
+
 }
