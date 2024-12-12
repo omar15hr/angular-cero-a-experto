706d4f89b4cdee1fd2596a6146018a6fcc12fef6
omar <omarhrn@gmail.com>
2024-12-12 08:59:48 -0300

    Inicio de pipes personalizados y creacion de componente order

--- 05-pipesApp/src/app/products/products.module.ts
+++ 05-pipesApp/src/app/products/products.module.ts
@@ -7,6 +7,7 @@ import { ProductsRoutingModule } from './products-routing.module';
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
+import { OrderComponent } from './pages/order/order.component';
 
 
 
@@ -14,7 +15,8 @@ import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.compo
   declarations: [
     BasicsPageComponent,
     NumbersPageComponent,
-    UncommonPageComponent
+    UncommonPageComponent,
+    OrderComponent
   ],
   imports: [
     CommonModule,
