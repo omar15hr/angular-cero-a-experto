d3123de9f0fdcc39a28d5801a5ec8c23ea138e7a
omar <omarhrn@gmail.com>
2024-12-16 18:47:55 -0300

    getters dentro del componente

--- 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Publisher } from '../../interfaces/hero.interface';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -23,10 +24,16 @@ export class NewPageComponent {
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
 
+  constructor( private heroesService: HeroesService ) {}
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
   onSubmit(): void {
-    console.log({
-      formIsValid: this.heroForm.valid,
-      value: this.heroForm.value,
-    })
+    if ( this.heroForm.invalid ) return;
+
+    // this.heroesService.updateHero( this.heroForm.value )
   }
 }
