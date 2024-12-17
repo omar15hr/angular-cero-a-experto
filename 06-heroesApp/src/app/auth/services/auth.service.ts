95e8e2a5440e94de235ceac61123037e57794319
omar <omarhrn@gmail.com>
2024-12-17 10:28:54 -0300

    Autenticar y mostrar usuario activo

--- 06-heroesApp/src/app/auth/services/auth.service.ts
+++ 06-heroesApp/src/app/auth/services/auth.service.ts
@@ -18,11 +18,16 @@ export class AuthService {
   }
 
   login( email: string, password: string ): Observable<User> {
-    this.http.get<User>(`${this.baseUrl}/users/1`)
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
       .pipe(
         tap( user => this.user = user ),
-        tap( user => localStorage.setItem('token', user.id.toString()) ),
+        tap( user => localStorage.setItem('token', 'sdiofj.qer232.232') ),
       )
+  }
+
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
   }
 
 }
--- 06-heroesApp/src/app/heroes/pages/layout-page/layout-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
+import { User } from '../../../auth/interfaces/user.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout-page',
@@ -13,4 +16,18 @@ export class LayoutPageComponent {
     { label: 'Buscar', icon: 'search', url: './search' },
   ]
 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  get user(): User | undefined {
+    return this.authService.currentUser;
+  }
+
+
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigateByUrl( '/auth/login' );
+  }
 }
