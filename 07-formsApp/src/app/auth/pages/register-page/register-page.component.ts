5b53434000b2dd186b6ec68e4b7f24f79e73e920
omar <omarhrn@gmail.com>
2024-12-19 11:59:28 -0300

    Formulario de registro

--- 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required ]],
+    email: ['', [ Validators.required ]],
+    username: ['', [ Validators.required ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required ]],
+    });
+
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  isValidField(field: string) {
+    
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 
 }
