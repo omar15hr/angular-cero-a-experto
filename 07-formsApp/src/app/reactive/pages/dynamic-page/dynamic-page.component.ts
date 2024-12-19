3e34f6c8752b118efcc34481924fe558a4532bda
omar <omarhrn@gmail.com>
2024-12-19 10:36:07 -0300

    Insertar elementos al formarray

--- 07-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
@@ -15,6 +15,8 @@ export class DynamicPageComponent {
       ['Death Stranding', Validators.required],
     ]),
   });
+
+  public newFavorite: FormControl = new FormControl('', Validators.required);
 
   constructor(
     private fb: FormBuilder
@@ -53,6 +55,17 @@ export class DynamicPageComponent {
     return null;
   }
 
+  onAddToFavorite(): void {
+    if ( this.newFavorite.invalid ) return;
+    const newGame = this.newFavorite.value;
+
+    this.favoriteGames.push(
+      this.fb.control( newGame, Validators.required )
+    )
+  
+    this.newFavorite.reset();
+  }
+
   onDeleteFavorite( index: number ): void {
     this.favoriteGames.removeAt(index);
   }
@@ -63,6 +76,7 @@ export class DynamicPageComponent {
       return;
     }
     console.log(this.myForm.value);
+    (this.myForm.controls['favoriteGames'] as FormArray) = this.fb.array([]);
     this.myForm.reset();
   }
 
