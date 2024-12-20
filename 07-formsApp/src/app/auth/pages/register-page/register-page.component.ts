022eed88ea903d7e8dc14332c51465ee3bc459c5
omar <omarhrn@gmail.com>
2024-12-20 09:57:06 -0300

    verificar que dos campos sean iguales

--- 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 // import * as customValidators from '../../../shared/validators/validators';
 import { ValidatorsService } from '../../../shared/services/validators.service';
+import { EmailValidatorService } from '../../../shared/validators/email-validator.service';
 
 @Component({
   selector: 'app-register-page',
@@ -12,15 +13,21 @@ export class RegisterPageComponent {
 
   public myForm: FormGroup = this.fb.group({
     name: ['', [ Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern) ]],
-    email: ['', [ Validators.required, Validators.pattern(this.validatorsService.emailPattern) ]],
+    // email: ['', [ Validators.required, Validators.pattern(this.validatorsService.emailPattern) ], [ new EmailValidatorService() ]],
+    email: ['', [ Validators.required, Validators.pattern(this.validatorsService.emailPattern) ], [ this.emailValidatorService ]],
     username: ['', [ Validators.required, this.validatorsService.cantBeStrider ]],
     password: ['', [ Validators.required, Validators.minLength(6) ]],
     password2: ['', [ Validators.required ]],
+    }, {
+      validators: [
+        this.validatorsService.isFieldOneEqualFieldTwo('password', 'password2')
+      ]
     });
 
   constructor(
     private fb: FormBuilder,
-    private validatorsService: ValidatorsService
+    private validatorsService: ValidatorsService,
+    private emailValidatorService: EmailValidatorService
   ) {}
 
   isValidField(field: string) {
