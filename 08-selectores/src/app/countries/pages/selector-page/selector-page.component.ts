532f0fade919fce1ed93371e5c020c5d3b50cede
omar <omarhrn@gmail.com>
2024-12-21 16:21:29 -0300

    Tercer selector anidado fronteras

--- 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
+++ 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interface';
-import { switchMap, tap } from 'rxjs';
+import { filter, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -12,11 +12,12 @@ import { switchMap, tap } from 'rxjs';
 export class SelectorPageComponent implements OnInit {
 
   public countriesByRegion: SmallCountry[] = [];
+  public borders: string[] = [];
 
   public myForm: FormGroup = this.fb.group({
     region : ['', Validators.required],
     country: ['', Validators.required],
-    borders: ['', Validators.required],
+    border : ['', Validators.required],
   })
 
   constructor(
@@ -27,6 +28,7 @@ export class SelectorPageComponent implements OnInit {
   ngOnInit(): void {
 
     this.onRegionChanged();
+    this.onCountryChanged();
     
   }
 
@@ -41,6 +43,7 @@ export class SelectorPageComponent implements OnInit {
     this.myForm.get('region')!.valueChanges
     .pipe(
       tap( () => this.myForm.get('country')!.setValue('') ),
+      tap( () => this.borders = [] ),
       switchMap( region => this.countriesService.getCountriesByRegion(region) )
     )
     .subscribe( countries => {
@@ -49,5 +52,19 @@ export class SelectorPageComponent implements OnInit {
 
   }
 
+
+  onCountryChanged(): void {
+
+    this.myForm.get('country')!.valueChanges
+    .pipe(
+      tap( () => this.myForm.get('border')!.setValue('') ),
+      filter( ( value: string ) => value.length > 0 ),
+      switchMap( (alphaCode) => this.countriesService.getCountryByAlphaCode(alphaCode) )
+    )
+    .subscribe( country => {
+      this.borders = country.borders;
+    });
+
+  }
 
 }
